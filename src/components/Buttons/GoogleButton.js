2aedaf067f1e15a09b42a65cb600509d3dee6309
Kirill <k.pirinen.rus@gmail.com>
2022-01-29 19:51:00 +0300

    secure ws switched

--- src/components/Buttons/GoogleButton.js
+++ src/components/Buttons/GoogleButton.js
@@ -1,11 +1,13 @@
 import styles from "./Buttons.module.scss"
 import GoogleLogin from 'react-google-login';
 
+const REACT_APP_GOOGLE_ID = '1062918467587-08js9mj4jovvslqhg5fjsmn9hfqh68ts.apps.googleusercontent.com'
+
 export const GoogleButton = (props) => {
   return (
     <GoogleLogin
     {...props}
-    clientId={process.env.REACT_APP_GOOGLE_ID}
+    clientId={REACT_APP_GOOGLE_ID}
     render={(renderProps) => 
       (
       <button {...renderProps} type="button" className={styles.google}>
