66a8ef86c01687bd910571cf74a8ac23e305ba3e
Kirill <k.pirinen.rus@gmail.com>
2022-02-12 19:02:20 +0300

    copy button added

--- src/pages/FormGenerated/FormGenerated.js
+++ src/pages/FormGenerated/FormGenerated.js
@@ -2,14 +2,14 @@ import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom";
 import { SimpleButton } from "../../components/Buttons/SimpleButton";
 import { MainLink } from "../../components/Links/MainLink";
-import { host } from "../../config/endPoints";
 import { deliverForm } from "../../redux/actions/createForm.ac";
+import { setInformer } from "../../redux/actions/Informer.ac";
 import styles from './FormGenerated.module.scss';
 
 export const FormGenerated = () => {
 
   const form = useSelector(state=>state.createForm)
-
+  const linkAddress = `https://easy2give.ru/sentform/${form.id}`
   const navigate = useNavigate()
   
   const dispatch = useDispatch()
@@ -17,15 +17,22 @@ export const FormGenerated = () => {
   const clickHandler = () => {
     dispatch(deliverForm(form.id, navigate))
   }
+
+  const copyHandler = () => {
+    navigator.clipboard.writeText(linkAddress)
+    dispatch(setInformer({info:'Ссылка скопирована'}))
+  }
   
+
   return (
     <>
       {form?.id ? <div className={styles.content}>
       <h3>Поздравляем, форма успешно сгенерирована</h3>
       <p>Ссылка на анкету для друга:</p> 
       <div className={styles.linkwrapper}>
-        <MainLink to={`/sentform/${form.id}`} text={`${host}/sentform/${form.id}`}/>
+        <MainLink to={`/sentform/${form.id}`} text={linkAddress}/>
       </div>
+      <SimpleButton type="button" onClick={copyHandler} text='Скопировать ссылку'/>
       <p>Можете отправить ссылку другу самостоятельно либо доверить отправку нам.
         Отправим анонимно, на почтовый ящик указанный Вами при заполнении контактных данных.
       </p>
